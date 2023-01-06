370e5a267b8194ccc5fea0fdcbd64077f1b92c53
Jimmy Ortega <fabricioortega2011@gmail.com>
2023-01-06 18:43:28 -0500

    Prueba 06/01/2023

---
+++ src/main/java/com/example/demo/ProyectoPaU2JoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaU2JoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaU2JoApplication.class, args);
+	}
+
+}
